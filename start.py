d63bbe61fe805ab1fd243b77911d67ab95c263b2
Claers <ferrereflorent@hotmail.fr>
2019-12-13 23:21:31 +0100

    cpu_count

--- start.py
+++ start.py
@@ -5,6 +5,7 @@ import bot
 import multiprocessing
 
 if __name__ == "__main__":
+    print(multiprocessing.cpu_count())
     server_p = multiprocessing.Process(target=server.launch)
     bot_p = multiprocessing.Process(target=bot.launch)
     server_p.start()
